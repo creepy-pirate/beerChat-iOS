61f3ac69ae461314d9ed7b949eca8a93b943ba8b
Deepak Singh <dv72276@gmail.com>
2021-06-18 02:50:06 +0530

    Fixed Cocoapods problems

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,27 +12,32 @@
 
 
 import UIKit
+import CLTypingLabel
 
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = ""
-        var charIndex = 0.0
-        let titleText = "🍺BeerChat"
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            
-            charIndex += 1
-        }
-
-       
-    }
+        titleLabel.text = "🍺BeerChat"
+        
+//
+//
+//        var charIndex = 0.0
+//        let titleText = ""
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//
+//            charIndex += 1
+//        }
+//
+//
+//    }
     
 
+}
 }
